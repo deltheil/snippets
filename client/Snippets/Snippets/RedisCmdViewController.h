d91fcc29a074906ce2c3ebf5778ad07518eb751c
Cédric Deltheil <cedric@moodstocks.com>
2013-10-19 22:13:47 +0200

    client: create a draft controller for the Redis cmd display

---
+++ client/Snippets/Snippets/RedisCmdViewController.h
@@ -0,0 +1,13 @@
+//
+//  RedisCmdViewController.h
+//  Snippets
+//
+//  Created by Cédric Deltheil on 19/10/13.
+//  Copyright (c) 2013 AppHACK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RedisCmdViewController : UIViewController
+
+@end

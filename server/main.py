b20672d512554a9274717b8d185eef99a18cc22d
Tushar Ghosh <2shar007@gmail.com>
2013-10-19 14:53:12 +0200

    server script entry point

---
+++ server/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# AngelHack Paris Oct '13
+# @uthors Tushar Ghosh (2shar007)
+
+import sys
+import urllib, urllib2
+
+def Run():
+	print "Test"
+	return
+
+if __name__ == "__main__":
+	Run()

9ea642561c9689e60447e2eb2d7fcf4a53d42ba7
Tushar Ghosh <2shar007@gmail.com>
2013-10-20 01:32:20 +0200

    converted .md to .html using Pandoc

--- server/Preprocess.py
+++ server/Preprocess.py
@@ -6,8 +6,21 @@
 import os
 import sys
 import json
+import subprocess
 import urllib, urllib2
 from Config import Config
+
+# Structure to hold data per command
+blob = {}
+
+
+# Read specified file
+def readFile(path):
+	if os.path.isfile(path):
+		with open(path, 'r') as sourceFile:
+			return sourceFile.read()
+	return None
+
 
 # Get file contents from specified directory
 def fetch(path, ext):
@@ -20,15 +33,9 @@ def fetch(path, ext):
 				data[entry] = fileContent
 	return data
 
-# Read specified file
-def readFile(path):
-	if os.path.isfile(path):
-		with open(path, 'r') as sourceFile:
-			return sourceFile.read()
-	return None
 
 # Sanitize JSON data
-def printKeyVals(data, indent=0):
+def sanitize(data, indent=0):
     if isinstance(data, list):
         print
         for item in data:
@@ -45,22 +52,56 @@ def printKeyVals(data, indent=0):
 def extractSummary(filePath):
 	summaryCmd = readFile(filePath)
 	if summaryCmd is not None:
-		cmdList = json.loads(summaryCmd)
-		for cmd, attr in cmdList.iteritems():
-			print cmd, attr
-		# TODO	
+		cmdList = {}
+		rawCmdList = json.loads(summaryCmd)
+		for cmd, rawAttr in rawCmdList.iteritems():
+			print cmd, rawAttr
+		#cmdList = sanitize(rawCmdList)
+		# TODO
+		return cmdList	
 	return None
 
-# Get command details from .md files
+# Execute pandoc command in python
+def pandoc(arg):
+	p = subprocess.Popen(['pandoc', '--to=html', '--from=markdown'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+	return p.communicate(arg)[0]
+
+# Get command details from .md files and extract CLI data and MarkDown
 def extractDetail(path):
 	detailCmd = fetch(path, ".md")
-	# TODO
+	for cmd, rawDetail in detailCmd.iteritems():
+		cli = []
+		markDown = ""
+		# parse to ge MarkDown and CLI data
+		detail = rawDetail.split("```")
+		for x in xrange(0, len(detail), 2):
+			markDown += detail[x]
+		markDown = markDown.replace("@","## ")
+		html = pandoc(markDown)
+		for x in xrange(1, len(detail), 2):
+			cliExmpl = detail[x]
+			if cliExmpl[:3] == "cli":
+				cli.append(cliExmpl[3:])
+			else:
+				cli.append(cliExmpl)
+		#generate html from markDown
+		if cmd not in blob:
+			blob[cmd] = {}
+		if "html" not in blob[cmd]:
+			blob[cmd]["html"] = ""
+		html = html + blob[cmd]["html"]
+		blob[cmd]["html"] = html
+		if "cli" not in blob[cmd]:
+			blob[cmd]["cli"] = []
+		cli = cli + blob[cmd]["cli"]
+		blob[cmd]["cli"] = cli
+		print blob[cmd]["html"]
 	return
 
 # Process Pull data to conform to Model.md
 def preprocess():
-	summary = extractSummary(Config.summary)
-	detail = extractDetail(Config.commands)
+	#summary = extractSummary(Config.summary)
+	extractDetail(Config.commands)
 	# TODO
         return
 

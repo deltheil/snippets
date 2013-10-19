b1d9249c0e6185f25fe326bdf088905e84f2e44b
Tushar Ghosh <2shar007@gmail.com>
2013-10-19 22:15:27 +0200

    ignore target and preprocess parses JSON

--- server/Preprocess.py
+++ server/Preprocess.py
@@ -5,6 +5,7 @@
 
 import os
 import sys
+import json
 import urllib, urllib2
 from Config import Config
 
@@ -26,11 +27,40 @@ def readFile(path):
 			return sourceFile.read()
 	return None
 
+# Sanitize JSON data
+def printKeyVals(data, indent=0):
+    if isinstance(data, list):
+        print
+        for item in data:
+            printKeyVals(item, indent+1)
+    elif isinstance(data, dict):
+        print
+        for k, v in data.iteritems():
+            print "    " * indent, k + ":",
+            printKeyVals(v, indent + 1)
+    else:
+        print data
+
+# Get command summaries
+def extractSummary(filePath):
+	summaryCmd = readFile(filePath)
+	if summaryCmd is not None:
+		cmdList = json.loads(summaryCmd)
+		for cmd, attr in cmdList.iteritems():
+			print cmd, attr
+		# TODO	
+	return None
+
+# Get command details from .md files
+def extractDetail(path):
+	detailCmd = fetch(path, ".md")
+	# TODO
+	return
 
 # Process Pull data to conform to Model.md
 def preprocess():
-	summaryCmd = readFile(Config.summary)
-	detailCmd = fetch(Config.commands, ".md")
-	
+	summary = extractSummary(Config.summary)
+	detail = extractDetail(Config.commands)
+	# TODO
         return
 

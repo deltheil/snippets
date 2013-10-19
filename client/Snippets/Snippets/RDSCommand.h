2b3c2b0cf5bf9880711ef945a18ae68dfb491d81
Cédric Deltheil <cedric@moodstocks.com>
2013-10-19 23:31:35 +0200

    client: add a scaffold model for Redis commands

---
+++ client/Snippets/Snippets/RDSCommand.h
@@ -0,0 +1,18 @@
+//
+//  RDSCommand.h
+//  Snippets
+//
+//  Created by Cédric Deltheil on 19/10/13.
+//  Copyright (c) 2013 AppHACK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RDSCommand : NSObject
+
+// e.g "GET"
+@property (nonatomic, copy, readonly) NSString *name;
+// e.g "Get the value of a key"
+@property (nonatomic, copy, readonly) NSString *summary;
+
+@end
---
+++ client/Snippets/Snippets/RDSCommand.m
@@ -0,0 +1,13 @@
+//
+//  RDSCommand.m
+//  Snippets
+//
+//  Created by Cédric Deltheil on 19/10/13.
+//  Copyright (c) 2013 AppHACK. All rights reserved.
+//
+
+#import "RDSCommand.h"
+
+@implementation RDSCommand
+
+@end

e0dadc09c446e82f701c0f890354071cccb1fcd3
Cédric Deltheil <cedric@moodstocks.com>
2013-10-19 23:52:01 +0200

    client: add RDSCommand database fetching

--- client/Snippets/Snippets/RDSCommand.h
+++ client/Snippets/Snippets/RDSCommand.h
@@ -10,6 +10,11 @@
 
 #import <Mantle.h>
 
+@class WNCDatabase;
+
+// rds:cmds
+extern NSString * const kRDSCommandsNS;
+
 @interface RDSCommand : NSObject
 
 // e.g "GET"
@@ -17,5 +22,10 @@
 // e.g "Get the value of a key"
 @property (nonatomic, copy, readonly) NSString *summary;
 // TODO: cli (array)
+
++ (void)setDatabase:(WNCDatabase *)database;
+
++ (NSArray *)fetch;
++ (NSArray *)fetch:(NSError **)error;
 
 @end
--- client/Snippets/Snippets/RDSCommand.m
+++ client/Snippets/Snippets/RDSCommand.m
@@ -8,6 +8,12 @@
 
 #import "RDSCommand.h"
 
+#import <Winch/Winch.h>
+
+static __weak WNCDatabase *_wnc_database;
+
+NSString * const kRDSCommandsNS = @"rds:cmds";
+
 @implementation RDSCommand
 
 + (NSDictionary *)JSONKeyPathsByPropertyKey
@@ -16,6 +22,55 @@
              @"name": @"name",
              @"summary": @"summary"
     };
+}
+
++ (NSArray *)fetch
+{
+    return [self fetch:nil];
+}
+
++ (void)setDatabase:(WNCDatabase *)database
+{
+    _wnc_database = database;
+}
+
++ (NSArray *)fetch:(NSError **)error
+{
+    WNCNamespace *ns = [_wnc_database getNamespace:kRDSCommandsNS];
+    NSMutableArray *cmds = [NSMutableArray array];
+    NSError *iterError = nil;
+    __block BOOL dataError = NO;
+    
+    [ns enumerateRecordsUsingBlock:^(NSString *key, NSMutableData *data, int *option) {
+        NSError *jsonError = nil;
+        id jsonDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+        if (jsonError) {
+            dataError = YES;
+            *option = kWNCIterStop;
+            return;
+        }
+        
+        NSError *parseError = nil;
+        RDSCommand *cmd = [MTLJSONAdapter modelOfClass:RDSCommand.class
+                                    fromJSONDictionary:jsonDict
+                                                 error:&parseError];
+        if (parseError) {
+            dataError = YES;
+            *option = kWNCIterStop;
+            return;
+        }
+        
+        [cmds addObject:cmd];
+    } error:&iterError];
+    
+    if (iterError || dataError) {
+        if (error) {
+            *error = [NSError errorWithDomain:@"RDSCommand" code:1 userInfo:nil];
+            return nil;
+        }
+    }
+    
+    return cmds;
 }
 
 @end

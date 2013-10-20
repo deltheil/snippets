44a66f12b013754859be1c20033c6d47edea92c7
Cédric Deltheil <cedric@moodstocks.com>
2013-10-20 03:16:12 +0200

    client: working Redis console
    Yet still work in progress

--- client/Snippets/Snippets/RedisConsoleViewController.m
+++ client/Snippets/Snippets/RedisConsoleViewController.m
@@ -8,6 +8,9 @@
 
 #import "RedisConsoleViewController.h"
 
+#import "Redis.h" // our in memory engine
+#import "NSError+Redis.h"
+
 @interface RedisConsoleViewController () <UITextFieldDelegate>
 
 @property (weak) IBOutlet UIWebView *webview;
@@ -15,15 +18,26 @@
 
 @end
 
-@implementation RedisConsoleViewController
+@implementation RedisConsoleViewController {
+    Redis *_redis;
+    NSMutableArray *_history; // use a Redis list instead?
+    NSMutableArray *_entries;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        _redis = [[Redis alloc] init];
+        _history = [[NSMutableArray alloc] init];
+        _entries = [[NSMutableArray alloc] init];
     }
     return self;
+}
+
+- (void)dealloc
+{
+    [_redis close];
 }
 
 - (void)viewDidLoad
@@ -46,8 +60,34 @@
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    NSLog(@"Redis command -> %@ ", textField.text);
+    NSError *error;
+    NSString *entry;
+    NSString *cmd = textField.text;
+    
+    // Run the command
+    NSString *resp = [_redis exec:cmd error:&error];
+    if (!error) {
+        entry = resp;
+    }
+    else {
+        entry = [[error rds_message] stringByReplacingOccurrencesOfString:@"Vedis" withString:@"Redis"];
+    }
+    
+    // Create the pair command + response / error
+    [_entries addObject:[NSString stringWithFormat:@"<strong>redis> %@</strong>", cmd]];
+    [_entries addObject:entry];
+
+    // Recreate the full HTML with all entries
+    NSString *html = [_entries componentsJoinedByString:@"<br/>"];
+    
+    [self.webview loadHTMLString:html baseURL:nil];
+    
+    // TODO: force the view to scroll down
+    
+    // Record the command into the history and clean up the prompt
+    [_history addObject:cmd];
     textField.text = @"";
+    
     return YES;
 }
 

0c9f132a50ec17d83efb7ede24fff4441c036d0b
Aymeric Gallissot <hello@aymericgallissot.fr>
2013-10-20 02:45:42 +0200

    Update command view

--- client/Snippets/Snippets/Controllers/CommandViewController.m
+++ client/Snippets/Snippets/Controllers/CommandViewController.m
@@ -10,6 +10,8 @@
 
 @interface CommandViewController ()
 
+@property (nonatomic, strong) UIWebView *webView;
+
 @end
 
 @implementation CommandViewController
@@ -26,13 +28,40 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+	
+    self.view.backgroundColor = [UIColor colorWithHexString:@"ffffff"];
+    
+    // WebView
+    self.webView = [[UIWebView alloc] initWithFrame:CGRectMake(0.0, 0.0, self.view.width, self.view.height - 76.0)];
+    self.webView.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:self.webView];
+    
+    // Load the HTML template in memory
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"redis_cmd" ofType:@"html"];
+    NSFileHandle *fileHandle = [NSFileHandle fileHandleForReadingAtPath:path];
+    NSString *tpl = [[NSString alloc] initWithData:[fileHandle readDataToEndOfFile] encoding:NSUTF8StringEncoding];
+    
+    // Add the specific content
+    NSString *html = [NSString stringWithFormat:tpl, [self getContent]];
+    
+    [self.webView loadHTMLString:html baseURL:nil];
+    
+    // Try Code
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// Emulate the local database get
+- (NSString *)getContent
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"append" ofType:@"html"];
+    NSFileHandle *fileHandle = [NSFileHandle fileHandleForReadingAtPath:path];
+    return [[NSString alloc] initWithData:[fileHandle readDataToEndOfFile] encoding:NSUTF8StringEncoding];
 }
 
 @end

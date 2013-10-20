38ad84b8e13106868a5d5492ceda49c5efd8d23a
Cédric Deltheil <cedric@moodstocks.com>
2013-10-20 02:19:02 +0200

    client: add a redis console scaffold (draft area)

---
+++ client/Snippets/Snippets/RedisConsoleViewController.h
@@ -0,0 +1,13 @@
+//
+//  RedisConsoleViewController.h
+//  Snippets
+//
+//  Created by Cédric Deltheil on 20/10/13.
+//  Copyright (c) 2013 AppHACK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RedisConsoleViewController : UIViewController
+
+@end
---
+++ client/Snippets/Snippets/RedisConsoleViewController.m
@@ -0,0 +1,38 @@
+//
+//  RedisConsoleViewController.m
+//  Snippets
+//
+//  Created by Cédric Deltheil on 20/10/13.
+//  Copyright (c) 2013 AppHACK. All rights reserved.
+//
+
+#import "RedisConsoleViewController.h"
+
+@interface RedisConsoleViewController ()
+
+@end
+
+@implementation RedisConsoleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

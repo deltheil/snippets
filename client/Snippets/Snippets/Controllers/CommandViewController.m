3d5e23908dcc9ed5a55bed22cdb8d430dac0d8b0
Aymeric Gallissot <hello@aymericgallissot.fr>
2013-10-20 02:30:38 +0200

    Add controller command and add app icon

---
+++ client/Snippets/Snippets/Controllers/CommandViewController.m
@@ -0,0 +1,38 @@
+//
+//  CommandViewController.m
+//  Snippets
+//
+//  Created by Aymeric Gallissot on 20/10/13.
+//  Copyright (c) 2013 AppHACK. All rights reserved.
+//
+
+#import "CommandViewController.h"
+
+@interface CommandViewController ()
+
+@end
+
+@implementation CommandViewController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

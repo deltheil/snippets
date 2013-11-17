526a2b481d18b948d2ef0e582b548600eefc441e
Cédric Deltheil <cedric@moodstocks.com>
2013-11-17 17:20:38 +0100

    client: create a Redis view controller (scaffold)

---
+++ client/Snippets/Snippets/RedisViewController.m
@@ -0,0 +1,38 @@
+//
+//  RedisViewController.m
+//  Snippets
+//
+//  Created by Cédric Deltheil on 17/11/13.
+//  Copyright (c) 2013 Snippets. All rights reserved.
+//
+
+#import "RedisViewController.h"
+
+@interface RedisViewController ()
+
+@end
+
+@implementation RedisViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

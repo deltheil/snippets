3558ea8c219fac4fd363cdae8be61229bb2a7e9e
James Heng <james.heng@epitech.eu>
2013-12-06 14:06:51 +0100

    redis vc: add group collectionViewCell

---
+++ client/Snippets/Snippets/RDSGroupCell.h
@@ -0,0 +1,16 @@
+//
+//  RDSGroupCell.h
+//  Snippets
+//
+//  Created by James on 12/6/13.
+//  Copyright (c) 2013 Snippets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDSGroupCell : UICollectionViewCell
+
+
+@property (strong, nonatomic) NSString *groupName;
+
+@end
---
+++ client/Snippets/Snippets/RDSGroupCell.m
@@ -0,0 +1,23 @@
+//
+//  RDSGroupCell.m
+//  Snippets
+//
+//  Created by James on 12/6/13.
+//  Copyright (c) 2013 Snippets. All rights reserved.
+//
+
+#import "RDSGroupCell.h"
+
+@implementation RDSGroupCell
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+    }
+    return self;
+}
+
+
+
+@end

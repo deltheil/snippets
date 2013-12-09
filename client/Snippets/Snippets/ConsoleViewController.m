5a275afb52dd847c57dc1a07d981c9e762f8f5b3
James Heng <james@moodstocks.com>
2013-12-09 17:37:11 +0100

    console vc: handle keyboard will change

--- client/Snippets/Snippets/ConsoleViewController.m
+++ client/Snippets/Snippets/ConsoleViewController.m
@@ -19,25 +19,31 @@
 
 #pragma mark - Life Cyle
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+//    [self.textField becomeFirstResponder];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillChange:)
+                                                 name:UIKeyboardWillChangeFrameNotification
+                                               object:nil];
+
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillChangeFrameNotification
+                                                  object:nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Actions
@@ -45,6 +51,31 @@
 - (IBAction)dismissViewController:(id)sender
 {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - Notifications
+
+- (void)keyboardWillChange:(NSNotification *)notification
+{
+    NSDictionary *userInfo = notification.userInfo;
+    UIViewAnimationCurve curve = [[userInfo valueForKey:UIKeyboardAnimationCurveUserInfoKey] intValue];
+    CGFloat duration = [[userInfo valueForKey:UIKeyboardAnimationDurationUserInfoKey] floatValue];
+
+    CGRect endFrame = [[userInfo valueForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    endFrame = [self.view convertRect:endFrame fromView:nil];
+
+    float height = self.inputContainerView.frame.size.height;
+    float y = (endFrame.origin.y > self.view.frame.size.height) ? self.view.frame.size.height : endFrame.origin.y - height;
+
+    [UIView animateWithDuration:duration
+                          delay:0.0f
+                        options:(UIViewAnimationOptions)curve
+                     animations:^{
+                         CGRect textFieldFrame = self.inputContainerView.frame;
+                         textFieldFrame.origin.y = y;
+                         self.inputContainerView.frame = textFieldFrame;
+                     }
+                     completion:nil];
 }
 
 @end

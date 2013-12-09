6931b0f4b15d0d8d1857db06f91851320fa38d90
James Heng <james@moodstocks.com>
2013-12-09 15:56:02 +0100

    add UITextField category

---
+++ client/Snippets/Snippets/Components/Category/UITextField+Snippets.h
@@ -0,0 +1,18 @@
+//
+//  UITextField+Snippets.h
+//  Snippets
+//
+//  Created by James Heng on 09/12/13.
+//  Copyright (c) 2013 Snippets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (Snippets)
+
+@property (nonatomic, copy) NSString *sn_fontName;
+
+@property (nonatomic, copy) NSString *sn_textHexColor;
+@property (nonatomic, copy) NSString *sn_placeHolderHexColor;
+
+@end
---
+++ client/Snippets/Snippets/Components/Category/UITextField+Snippets.m
@@ -0,0 +1,44 @@
+//
+//  UITextField+Snippets.m
+//  Snippets
+//
+//  Created by James Heng on 09/12/13.
+//  Copyright (c) 2013 Snippets. All rights reserved.
+//
+
+#import "UITextField+Snippets.h"
+#import "UIColor+Snippets.h"
+
+@implementation UITextField (Snippets)
+
+- (NSString *)sn_fontName
+{
+    return self.font.fontName;
+}
+
+- (void)setSn_fontName:(NSString *)sn_fontName
+{
+    self.font = [UIFont fontWithName:sn_fontName size:self.font.pointSize];
+}
+
+- (NSString *)sn_textHexColor
+{
+    return [self sn_textHexColor];
+}
+
+- (void)setSn_textHexColor:(NSString *)sn_textHexColor
+{
+    [self setTextColor:[UIColor colorWithHexString:sn_textHexColor alpha:1]];
+}
+
+- (NSString *)sn_placeHolderHexColor
+{
+    return [self sn_placeHolderHexColor];
+}
+
+- (void)setSn_placeHolderHexColor:(NSString *)sn_placeHolderHexColor
+{
+    [self setValue:[UIColor colorWithHexString:sn_placeHolderHexColor alpha:1] forKeyPath:@"_placeholderLabel.textColor"];
+}
+
+@end

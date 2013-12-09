42faa67589f7b67ee1c87526326147bc6e7133d8
James Heng <james@moodstocks.com>
2013-12-09 10:58:02 +0100

    add command view controller class

---
+++ client/Snippets/Snippets/CommandViewController.h
@@ -0,0 +1,13 @@
+//
+//  CommandViewController.h
+//  Snippets
+//
+//  Created by James Heng on 09/12/13.
+//  Copyright (c) 2013 Snippets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommandViewController : UIViewController
+
+@end
---
+++ client/Snippets/Snippets/CommandViewController.m
@@ -0,0 +1,38 @@
+//
+//  CommandViewController.m
+//  Snippets
+//
+//  Created by James Heng on 09/12/13.
+//  Copyright (c) 2013 Snippets. All rights reserved.
+//
+
+#import "CommandViewController.h"
+
+@interface CommandViewController ()
+
+@end
+
+@implementation CommandViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

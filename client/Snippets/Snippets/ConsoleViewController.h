1b7496aa2779e02f4733a85fb4d4d114c78e850b
James Heng <james@moodstocks.com>
2013-12-09 12:19:30 +0100

    add console view controller class

---
+++ client/Snippets/Snippets/ConsoleViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConsoleViewController.h
+//  Snippets
+//
+//  Created by James Heng on 09/12/13.
+//  Copyright (c) 2013 Snippets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConsoleViewController : UIViewController
+
+@end
---
+++ client/Snippets/Snippets/ConsoleViewController.m
@@ -0,0 +1,40 @@
+//
+//  ConsoleViewController.m
+//  Snippets
+//
+//  Created by James Heng on 09/12/13.
+//  Copyright (c) 2013 Snippets. All rights reserved.
+//
+
+#import "ConsoleViewController.h"
+
+@interface ConsoleViewController ()
+
+@end
+
+@implementation ConsoleViewController
+
+#pragma mark - Life Cyle
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

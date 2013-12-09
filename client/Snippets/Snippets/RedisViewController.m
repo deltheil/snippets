5674b558a22aea0b6d598f1f7099420960a71133
James Heng <james@moodstocks.com>
2013-12-09 11:05:57 +0100

    redis vc: set segue identifier and prepareToSegue

--- client/Snippets/Snippets/RedisViewController.m
+++ client/Snippets/Snippets/RedisViewController.m
@@ -54,6 +54,11 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    
+}
+
 #pragma mark - Private
 
 - (NSArray *)groups

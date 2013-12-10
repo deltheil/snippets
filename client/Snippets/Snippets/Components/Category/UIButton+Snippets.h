c1cffeeb6d0b5f87ae4f006a2ef9b856172a035e
James Heng <james.heng@epitech.eu>
2013-12-11 00:07:11 +0100

    add UIButton ui category

---
+++ client/Snippets/Snippets/Components/Category/UIButton+Snippets.h
@@ -0,0 +1,16 @@
+//
+//  UIButton+Snippets.h
+//  Overlay
+//
+//  Created by James Heng on 27/11/13.
+//  Copyright (c) 2013 Moodstocks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (Snippets)
+
+@property (nonatomic, copy) NSString *sn_fontName;
+@property (nonatomic, copy) NSString *sn_hexColor;
+
+@end
---
+++ client/Snippets/Snippets/Components/Category/UIButton+Snippets.m
@@ -0,0 +1,35 @@
+//
+//  UIButton+Snippets.m
+//  Overlay
+//
+//  Created by James Heng on 27/11/13.
+//  Copyright (c) 2013 Moodstocks. All rights reserved.
+//
+
+#import "UIButton+Snippets.h"
+
+#import "UIColor+Snippets.h"
+
+@implementation UIButton (Snippets)
+
+- (NSString *)sn_fontName
+{
+    return self.titleLabel.font.fontName;
+}
+
+- (void)setSn_fontName:(NSString *)sn_fontName
+{
+    self.titleLabel.font = [UIFont fontWithName:sn_fontName size:self.titleLabel.font.pointSize];
+}
+
+- (NSString *)sn_hexColor
+{
+    return [self sn_hexColor];
+}
+
+- (void)setSn_hexColor:(NSString *)sn_hexColor
+{
+    [self setTitleColor:[UIColor colorWithHexString:sn_hexColor alpha:1] forState:UIControlStateNormal];
+}
+
+@end

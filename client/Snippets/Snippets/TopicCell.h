31b47209552b9a0ffc1ece74bb7be77bc8290084
James Heng <james@moodstocks.com>
2013-12-10 17:06:02 +0100

    add topic table view cell class

---
+++ client/Snippets/Snippets/TopicCell.h
@@ -0,0 +1,13 @@
+//
+//  TopicCell.h
+//  Snippets
+//
+//  Created by James Heng on 10/12/13.
+//  Copyright (c) 2013 Snippets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopicCell : UITableViewCell
+
+@end
---
+++ client/Snippets/Snippets/TopicCell.m
@@ -0,0 +1,28 @@
+//
+//  TopicCell.m
+//  Snippets
+//
+//  Created by James Heng on 10/12/13.
+//  Copyright (c) 2013 Snippets. All rights reserved.
+//
+
+#import "TopicCell.h"
+
+@implementation TopicCell
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

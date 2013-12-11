0a6a472d1a1615bbba90dbda74f3529bfc7cf5ec
James Heng <james@moodstocks.com>
2013-12-11 14:40:38 +0100

    add about vc class and link with storyboard

---
+++ client/Snippets/Snippets/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  Snippets
+//
+//  Created by James Heng on 11/12/13.
+//  Copyright (c) 2013 Snippets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ client/Snippets/Snippets/AboutViewController.m
@@ -0,0 +1,31 @@
+//
+//  AboutViewController.m
+//  Snippets
+//
+//  Created by James Heng on 11/12/13.
+//  Copyright (c) 2013 Snippets. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+#pragma mark - Life Cycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
